6e8fc03f185232eaaf7151ac7c042f625ce9a5e9
azevedorafael <rafaelsilva.fatec@gmail.com>
2018-11-09 00:18:06 -0200

    Add flatMap in Array prototype

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,5 @@
 import { handleStatus, log } from './utils/promise-helpers.js';
+import './utils/array-helpers.js';
 
 document
     .querySelector('#myButton')
@@ -6,7 +7,7 @@ document
         fetch('http://localhost:3000/notas')
             .then(handleStatus)
             .then(log)
-            .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
+            .then(notas => notas.$flatMap(nota => nota.itens))
             .then(log)
             .then(itens => itens.filter(item => item.codigo == '2143'))
             .then(log)
