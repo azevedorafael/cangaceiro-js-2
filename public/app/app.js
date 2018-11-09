752aaffc8d409d72fbafcac17c547e4b49d850da
azevedorafael <rafaelsilva.fatec@gmail.com>
2018-11-08 23:53:06 -0200

    Add log function

--- public/app/app.js
+++ public/app/app.js
@@ -1,24 +1,15 @@
-import { handleStatus } from './utils/promise-helpers.js';
+import { handleStatus, log } from './utils/promise-helpers.js';
 
 document
     .querySelector('#myButton')
     .onclick = () =>
         fetch('http://localhost:3000/notas')
             .then(handleStatus)
-            .then(itens => {
-                console.log(itens);
-                return itens;
-            })
+            .then(log)
             .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
-            .then(itens => {
-                console.log(itens);
-                return itens;
-            })
+            .then(log)
             .then(itens => itens.filter(item => item.codigo == '2143'))
-            .then(itens => {
-                console.log(itens);
-                return itens;
-            })
+            .then(log)
             .then(itens => itens.reduce((total, item) => total + item.valor, 0))
-            .then(console.log)
-            .catch(console.log)
+            .then(log)
+            .catch(log)
