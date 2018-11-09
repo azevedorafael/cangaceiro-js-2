f5c172793e018dd64a7849cfa678f71b7a77e33d
azevedorafael <rafaelsilva.fatec@gmail.com>
2018-11-08 23:47:50 -0200

    Fix multidimensioal array

--- public/app/app.js
+++ public/app/app.js
@@ -5,12 +5,20 @@ document
     .onclick = () =>
         fetch('http://localhost:3000/notas')
             .then(handleStatus)
-            .then(notas => notas.map(nota => nota.itens))
+            .then(itens => {
+                console.log(itens);
+                return itens;
+            })
+            .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
             .then(itens => {
                 console.log(itens);
                 return itens;
             })
             .then(itens => itens.filter(item => item.codigo == '2143'))
+            .then(itens => {
+                console.log(itens);
+                return itens;
+            })
             .then(itens => itens.reduce((total, item) => total + item.valor, 0))
             .then(console.log)
             .catch(console.log)
