0024795a67e53411917986c0d392866af9c1fe51
azevedorafael <rafaelsilva.fatec@gmail.com>
2018-11-11 19:45:48 -0200

    Remove logs

--- public/app/app.js
+++ public/app/app.js
@@ -6,11 +6,9 @@ document
     .onclick = () =>
         fetch('http://localhost:3000/notas')
             .then(handleStatus)
-            .then(log)
-            .then(notas => notas.$flatMap(nota => nota.itens))
-            .then(log)
-            .then(itens => itens.filter(item => item.codigo == '2143'))
-            .then(log)
-            .then(itens => itens.reduce((total, item) => total + item.valor, 0))
-            .then(log)
-            .catch(log)
+            .then(notas => notas
+                .$flatMap(nota => nota.itens)
+                .filter(item => item.codigo == '2143')
+                .reduce((total, item) => total + item.valor, 0))
+                .then(log)
+                .catch(log);
